33035ef2a9bb27b70dda8649c928405aa08ca423
fdfyzl131402 <2043399264@qq.com>
2022-12-13 18:24:33 +0800

    foundation

---
+++ 17_基础练习题/yzl_16_去掉空格.py
@@ -0,0 +1,9 @@
+a = "  Welcome to my world  "
+
+print(a.strip(" "))
+print(a.lstrip(" "))
+print(a.rstrip(" "))
+# 替换所有空格
+print(a.replace(" ", ""))
+print(a.split(" "))
+print("".join(a.split(" ")))
